cbbbab56a1ab879a30a8f09c562218f4a6a89fd1
XX989tmx <issacgravity8@gmail.com>
2021-01-08 04:59:18 +0900

    add sql connection

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-const express = require('express');
+const express = require("express");
+const database = require("./utils/database");
+
 const app = express();
-const ejs = require('ejs')
-const bodyParser = require('body-parser');
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
 app.set("view engine", "ejs");
 app.set("views", "./views");
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"));
 
-app.get('/inbox',(req,res) => {
-    res.render('inbox');
+app.get("/inbox", (req, res) => {
+  res.render("inbox");
 });
+// database.getConnection((err) => {
+//     if (err) console.log(err);
+//     connection.release();
+// })
+database
+  .execute("SELECT * FROM todo")
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.use((req,res,next) => {
-    res.status(404).render('404');
+app.use((req, res, next) => {
+  res.status(404).render("404");
 });
 
 app.listen(3000);
