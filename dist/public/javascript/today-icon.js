ff2100c80b8ce61fa3d9e9742e7e0662c2a21ea2
XX989tmx <issacgravity8@gmail.com>
2021-01-30 19:42:04 +0900

    modify

--- dist/public/javascript/today-icon.js
+++ dist/public/javascript/today-icon.js
@@ -1,45 +1,51 @@
 "use strict";
-// const setAsTodayIconsList = document.querySelectorAll(".setAsTodayIcon");
-// console.log('today');
-// console.log(setAsTodayIconsList);
-// for (let i = 0; i < setAsTodayIconsList.length; i++) {
-//     const setAsTodayIcon = setAsTodayIconsList[i];
-//     setAsTodayIcon.addEventListener('click',(event) => {
-//         // send http req
-//         // set isDone property as true in server side
-//         // if response 200
-//         console.log('ababababa');
-//         setAsTodayIcon.classList.add("today-icon-active");
-//         // remove li node
-//         setAsTodayIcon.parentNode?.removeChild(setAsTodayIcon);
-//     })
-// }
-var setAsTodayUl = document.getElementById("todo-container");
-var setAsTodayLiList = setAsTodayUl === null || setAsTodayUl === void 0 ? void 0 : setAsTodayUl.children;
+var setAsTodayIconsList = document.querySelectorAll(".setAsTodayIcon");
+console.log("today");
+console.log(setAsTodayIconsList);
 var _loop_1 = function (i) {
-    var setAsTodayLi = setAsTodayLiList[i];
-    setAsTodayLi.addEventListener("click", function (event) {
-        var todoItemsRow = setAsTodayLi.children;
-        var todoItemCols = todoItemsRow[0].children;
-        console.log(todoItemCols);
-        var todayIconDiv = todoItemCols[3];
-        console.log(todayIconDiv);
-        var todayIcon = todayIconDiv.children[0];
-        console.log(todayIcon);
+    var setAsTodayIcon = setAsTodayIconsList[i];
+    setAsTodayIcon.addEventListener("click", function (event) {
+        var _a, _b;
         // send http req
         // set isDone property as true in server side
-        // if response status code === 200
-        todayIcon.classList.add("today-icon-active");
+        // if response 200
+        console.log("ababababa");
+        setAsTodayIcon.classList.add("today-icon-active");
+        // bubble up to li element
+        var LiElement = (_b = (_a = setAsTodayIcon.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.parentElement;
+        console.log(LiElement);
         // remove li node
         setTimeout(function () {
             var _a;
-            (_a = setAsTodayLi.parentNode) === null || _a === void 0 ? void 0 : _a.removeChild(setAsTodayLi);
+            (_a = LiElement.parentNode) === null || _a === void 0 ? void 0 : _a.removeChild(LiElement);
         }, 1000);
     });
 };
-for (var i = 0; i < setAsTodayLiList.length; i++) {
+for (var i = 0; i < setAsTodayIconsList.length; i++) {
     _loop_1(i);
 }
+// const setAsTodayUl = document.getElementById("todo-container");
+// const setAsTodayLiList = setAsTodayUl?.children;
+// for (let i = 0; i < setAsTodayLiList.length; i++) {
+//   const setAsTodayLi = setAsTodayLiList[i];
+//   setAsTodayLi.addEventListener("click", (event) => {
+//     const todoItemsRow = setAsTodayLi.children;
+//     const todoItemCols = todoItemsRow[0].children;
+//     console.log(todoItemCols);
+//     const todayIconDiv = todoItemCols[3];
+//     console.log(todayIconDiv);
+//     const todayIcon = todayIconDiv.children[0];
+//     console.log(todayIcon);
+//     // send http req
+//             // set isDone property as true in server side
+//             // if response status code === 200
+//     todayIcon.classList.add("today-icon-active");
+//     // remove li node
+//     setTimeout(() => {
+//         setAsTodayLi.parentNode?.removeChild(setAsTodayLi);
+//     }, 1000);
+//   });
+// }
 // const todayIcon = document.getElementById("set-as-today-icon");
 // todayIcon?.addEventListener("click", (event) => {
 //   console.log("today");
--- src/public/javascript/today-icon.ts
+++ src/public/javascript/today-icon.ts
@@ -1,48 +1,52 @@
-// const setAsTodayIconsList = document.querySelectorAll(".setAsTodayIcon");
-// console.log('today');
-// console.log(setAsTodayIconsList);
+const setAsTodayIconsList:NodeListOf<Element> = document.querySelectorAll(".setAsTodayIcon");
+console.log("today");
+console.log(setAsTodayIconsList);
 
-// for (let i = 0; i < setAsTodayIconsList.length; i++) {
-//     const setAsTodayIcon = setAsTodayIconsList[i];
-//     setAsTodayIcon.addEventListener('click',(event) => {
-//         // send http req
-//         // set isDone property as true in server side
-//         // if response 200
-//         console.log('ababababa');
-
-//         setAsTodayIcon.classList.add("today-icon-active");
-//         // remove li node
-//         setAsTodayIcon.parentNode?.removeChild(setAsTodayIcon);
-
-//     })
-
-// }
-
-const setAsTodayUl = document.getElementById("todo-container");
-const setAsTodayLiList = setAsTodayUl?.children;
-
-for (let i = 0; i < setAsTodayLiList.length; i++) {
-  const setAsTodayLi = setAsTodayLiList[i];
-  setAsTodayLi.addEventListener("click", (event) => {
-    const todoItemsRow = setAsTodayLi.children;
-    const todoItemCols = todoItemsRow[0].children;
-    console.log(todoItemCols);
-    const todayIconDiv = todoItemCols[3];
-    console.log(todayIconDiv);
-    const todayIcon = todayIconDiv.children[0];
-    console.log(todayIcon);
-
+for (let i = 0; i < setAsTodayIconsList.length; i++) {
+  const setAsTodayIcon = setAsTodayIconsList[i];
+  setAsTodayIcon.addEventListener("click", (event) => {
     // send http req
-            // set isDone property as true in server side
-            // if response status code === 200
-    todayIcon.classList.add("today-icon-active");
+    // set isDone property as true in server side
+    // if response 200
+    console.log("ababababa");
+
+    setAsTodayIcon.classList.add("today-icon-active");
+    // bubble up to li element
+    const LiElement: HTMLElement =
+      setAsTodayIcon.parentElement?.parentElement?.parentElement;
+    console.log(LiElement);
     // remove li node
     setTimeout(() => {
-        setAsTodayLi.parentNode?.removeChild(setAsTodayLi);
+      LiElement.parentNode?.removeChild(LiElement);
     }, 1000);
-    
   });
 }
+
+// const setAsTodayUl = document.getElementById("todo-container");
+// const setAsTodayLiList = setAsTodayUl?.children;
+
+// for (let i = 0; i < setAsTodayLiList.length; i++) {
+//   const setAsTodayLi = setAsTodayLiList[i];
+//   setAsTodayLi.addEventListener("click", (event) => {
+//     const todoItemsRow = setAsTodayLi.children;
+//     const todoItemCols = todoItemsRow[0].children;
+//     console.log(todoItemCols);
+//     const todayIconDiv = todoItemCols[3];
+//     console.log(todayIconDiv);
+//     const todayIcon = todayIconDiv.children[0];
+//     console.log(todayIcon);
+
+//     // send http req
+//             // set isDone property as true in server side
+//             // if response status code === 200
+//     todayIcon.classList.add("today-icon-active");
+//     // remove li node
+//     setTimeout(() => {
+//         setAsTodayLi.parentNode?.removeChild(setAsTodayLi);
+//     }, 1000);
+
+//   });
+// }
 
 // const todayIcon = document.getElementById("set-as-today-icon");
 
